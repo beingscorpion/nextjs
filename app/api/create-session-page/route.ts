57946f17c7ecce0dbcbfeb7c300dab932443522e
Faakhir <syedfaakhirabid@gmail.com>
2025-07-28 23:55:58 +0500

    checkout page removed

--- app/api/create-session-page/route.ts
+++ app/api/create-session-page/route.ts
@@ -29,9 +29,7 @@ import Stripe from "stripe";
 import { NextResponse } from "next/server";
 // import home from "page.tsx";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2025-06-30.basil",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export async function POST() {
   try {
